a874325575cb879ff4365e67298d1f8355b9cc10
minyez <minyez@iphy.ac.cn>
2022-08-12 19:39:37 +0800

    Add LiF k333 example

---
+++ examples/lif_k333/run_mi.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+exe="../../build/abf_trans.exe"
+
+[[ ! -x $exe ]] && echo "Executable not built in ../../build/" && exit 1
+
+cat > cell.txt << EOF
+0.00000000    2.01350000    2.01350000
+2.01350000    0.00000000    2.01350000
+2.01350000    2.01350000    0.00000000
+2
+0.000000000     0.000000000     0.000000000 3
+0.500000000     0.500000000     0.500000000 9
+EOF
+
+# the basis set corresponding to minimal+d of Li and minimal basis of F
+# Li
+#   valence      2  s   1.
+# #  hydro 2 p 1.6
+# #  hydro 2 s 2
+#    hydro 3 d 2.6
+#
+# F
+#   valence      2  s   2.
+#   valence      2  p   5.
+# #  hydro 2 p 1.7
+# #  hydro 3 d 7.4
+# #  hydro 3 s 6.8
+
+# Note the following is the auxiliary basis, not the original basis
+# ABFs on Li = 38, F = 18
+cat > basis_id.txt << EOF
+3 0 1
+3 2 1
+3 0 1
+3 2 1
+3 0 1
+3 4 1
+3 3 1
+3 2 1
+3 1 1
+3 0 1
+9 0 2
+9 1 1
+9 0 1
+9 1 1
+9 2 1
+9 1 1
+9 0 1
+EOF
+
+rm -f abf_trans_out*
+
+# the coordinates of corresponding k indices are
+
+#  1:  0.0   0.0   0.0 
+#  2:  0.0   0.0   1/3 
+#  3:  0.0   0.0   2/3 
+#  4:  0.0   1/3   0.0 
+#  5:  0.0   1/3   1/3 
+#  6:  0.0   1/3   2/3 
+#  7:  0.0   2/3   0.0 
+#  8:  0.0   2/3   1/3 
+#  9:  0.0   2/3   2/3 
+# 10:  1/3   0.0   0.0 
+# 11:  1/3   0.0   1/3 
+# 12:  1/3   0.0   2/3 
+# 13:  1/3   1/3   0.0 
+# 14:  1/3   1/3   1/3 
+# 15:  1/3   1/3   2/3 
+# 16:  1/3   2/3   0.0 
+# 17:  1/3   2/3   1/3 
+# 18:  1/3   2/3   2/3 
+# 19:  2/3   0.0   0.0 
+# 20:  2/3   0.0   1/3 
+# 21:  2/3   0.0   2/3 
+# 22:  2/3   1/3   0.0 
+# 23:  2/3   1/3   1/3 
+# 24:  2/3   1/3   2/3 
+# 25:  2/3   2/3   0.0 
+# 26:  2/3   2/3   1/3 
+# 27:  2/3   2/3   2/3
+
+$exe cell.txt basis_id.txt matrix_inputs.txt
+
+# $exe cell.txt basis_id.txt matrix_inputs_all.txt
--- src/sph.h
+++ src/sph.h
@@ -7,18 +7,19 @@
 // real spherical harmonics used in the original Blanco's paper
 inline cplxdb get_C_matrix_element_orig(int m, int mp)
 {
+    const double sqrt2 = M_SQRT2;
     if (abs(m) == abs(mp))
     {
         if (m == 0)
             return cplxdb(1.0, 0.0);
         if (m > 0 && mp > 0)
-            return cplxdb(std::pow(-1.0, m)/std::sqrt(2.0), 0.0);
+            return cplxdb(std::pow(-1.0, m)/sqrt2, 0.0);
         if (m > 0 && mp < 0)
-            return cplxdb(1.0/std::sqrt(2.0), 0.0);
+            return cplxdb(1.0/sqrt2, 0.0);
         if (m < 0 && mp > 0)
-            return cplxdb(0, -std::pow(-1, mp)/std::sqrt(2.0));
+            return cplxdb(0, -std::pow(-1, mp)/sqrt2);
         if (m < 0 && mp < 0)
-            return cplxdb(0, 1.0/std::sqrt(2.0));
+            return cplxdb(0, 1.0/sqrt2);
     }
     return cplxdb(0., 0.);
 }
@@ -26,18 +27,35 @@ inline cplxdb get_C_matrix_element_orig(int m, int mp)
 // real spherical harmonics used in the aims code
 inline cplxdb get_C_matrix_element_aims(int m, int mp)
 {
+    const double sqrt2 = M_SQRT2;
     if (abs(m) == abs(mp))
     {
         if (m == 0)
             return cplxdb(1.0, 0.0);
         if (m > 0 && mp > 0)
-            return cplxdb(1.0/std::sqrt(2.0), 0.0);
+            return cplxdb(1.0/sqrt2, 0.0);
         if (m > 0 && mp < 0)
-            return cplxdb(std::pow(-1.0, m)/std::sqrt(2.0), 0.0);
+            return cplxdb(std::pow(-1.0, m)/sqrt2, 0.0);
         if (m < 0 && mp > 0)
-            return cplxdb(0, std::pow(-1, mp)/std::sqrt(2.0));
+            return cplxdb(0, -std::pow(-1, mp)/sqrt2);
         if (m < 0 && mp < 0)
-            return cplxdb(0, -1.0/std::sqrt(2.0));
+            return cplxdb(0, 1.0/sqrt2);
+
+        // original code from src/sym_base.f90
+        // if ((abs(m).ne.m).and. (abs(m2) .ne. m2))then
+        //     ! upper left, m<0 + m2<0
+        //     element = dcmplx(0.d0,1.d0)
+        // elseif ((abs(m) == m) .and. (abs(m2) .ne. m2))then
+        //     ! lower left, m>0 + m2<0
+        //     element = dcmplx(1.d0,0.d0)
+        // elseif ((abs(m) == m) .and. (abs(m2) == m2))then
+        //     ! lower right, m>0 + m2>0
+        //     element = dcmplx((-1)**(l-(l-m)),0.d0)
+        // elseif (.not.(abs(m).eq.m) .and. (abs(m2).eq.m2))then
+        //     ! upper right, m<0 + m2 > 0
+        //     element = -dcmplx(0.d0,(-1.d0)**(l-(l-m)))
+        // endif
+        // and matmul with its T matrix
     }
     return cplxdb(0., 0.);
 }
